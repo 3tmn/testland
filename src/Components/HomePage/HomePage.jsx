896558be286177699e668895044f065c62a4676f
Othmane RAZZOUQ <3tmn@users.noreply.github.com>
2025-11-21 20:05:56 +0100

    Slight home page modifications
    Descriptive text and view all button added

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -3,23 +3,31 @@ import styles from "./HomePage.module.css";
 
 const HomePage = () => {
   return (
-    <div className={styles.container}>
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
-      <ProductCard />
+    <div>
+      <div className={styles.textContainer}>
+        1 OF 1 VINTAGE UPCYCLED FLANNELS
+      </div>
+      <div className={styles.cardsContainer}>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+      <div className={styles.buttonContainer}>
+        <div className={styles.button}>View all</div>
+      </div>
     </div>
   );
 };
